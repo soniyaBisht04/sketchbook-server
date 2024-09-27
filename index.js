a015e1d297fc7a4a1bfcb4b7f671cb030177b256
Soniya Bisht <soniya@Soniyas-MacBook-Pro.local>
2024-09-27 23:30:32 +0200

    feat: update prod config

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(cors({origin: 'http://localhost:3000'}));
+const isDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3000' : 'https://sketchbook-mu.vercel.app';
+app.use(cors({origin: URL}));
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: 'http://localhost:3000' });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
   // ...
